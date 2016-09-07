6de218443ea09d4d575839cdbaf1af5d4b78f8b1
stijnvanhoey <stijnvanhoey@gmail.com>
2016-09-07 16:05:53 +0200

    add animalia plantae divided graph

--- src/grafiek_pathways/grafiek_pathways.R
+++ src/grafiek_pathways/grafiek_pathways.R
@@ -8,7 +8,7 @@ library(grid)
 setwd("~/githubs/inbo_alien-species-checklist/src/grafiek_pathways")
 
 dataset <- read.csv(
-    "../../data/interim/pathway_figure_data.tsv",
+    "../../data/interim/pathway_figure_aggregate_all.tsv",
     sep = "\t"
 )
 
@@ -61,3 +61,51 @@ ggplot(dataset, aes(x = reorder(subcategory, -count_species_key),
 ggsave("pathway_graph.png", width = 30, height = 33,
        units = "cm", dpi = 150)
 
+# -------------------------------------------------------
+
+dataset <- read.csv(
+    "../../data/interim/pathway_figure_aggregate_plant_animal.tsv",
+    sep = "\t"
+)
+
+# Make category capital letters
+dataset <- mutate_at(dataset, .cols = c("category"), .funs = toupper)
+# subcategory add line splits
+max.length.allowed <- 30
+dataset$subcategory <- as.factor(sapply(dataset$subcategory,
+                                        FUN = label.splitter,
+                                        max.length.allowed))
+# category add line splits
+max.length.allowed <- 20
+dataset$category <- as.factor(sapply(dataset$category,
+                                     FUN = label.splitter,
+                                     max.length.allowed))
+
+# Create the graph
+ggplot(dataset, aes(x = reorder(subcategory, -count_species_key),
+                    y = count_species_key, fill = kingdom)) +
+    geom_bar(stat = "identity", width = .65) +
+    geom_text(aes(label = count_species_key), y = .1, vjust = 0.5,
+              size = 4, angle = 90, hjust = 1.2) +
+    ylab("Number of species") +
+    facet_grid(. ~ category, scales = "free_x",
+               space = "free_x") +
+    theme_inbo2015(base_size = 11) +
+    theme(
+        axis.text.x = element_text(angle = 90, hjust = 1,
+                                   vjust = 0.2, size = 10),
+        axis.title.x = element_blank(),
+        axis.text.y = element_text(size = 12, angle = 90,
+                                   hjust = 0.5),
+        axis.title.y = element_text(size = 14),
+        strip.text.x = element_text(angle = 90, size = 12),
+        legend.direction = "vertical",
+        legend.position = "left",
+        legend.text = element_text(angle = 90, size = 14,
+                                   hjust = 0.5, vjust = 0.5),
+        legend.title = element_blank(),
+        legend.text.align = 0.5
+    )
+
+ggsave("pathway_graph_plant_animal.png", width = 30, height = 33,
+       units = "cm", dpi = 150)
