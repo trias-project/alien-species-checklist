ac7f0093b4c6955017e540a0defbe8a260980c8f
stijnvanhoey <stijnvanhoey@gmail.com>
2016-09-09 00:18:48 +0200

    update figure of cumulative count with full R version and correct transformation

--- src/grafiek_pathways/grafiek_pathways.R
+++ src/grafiek_pathways/grafiek_pathways.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(INBOtheme) # install with devtools: install_github("inbo/inboTheme")
 library(tidyr)
 library(grid)
+library(zoo)
 
 # gebruik een RStudio project! Dan heb je geen setwd() nodig
 setwd("~/githubs/inbo_alien-species-checklist/src/grafiek_pathways")
@@ -114,26 +115,114 @@ checklist <- read.csv(
     sep = "\t"
 )
 
-# drop the rows without habitat info
-checklist <- checklist[checklist$habitat_short != "",]
+habitats = c("freshwater", "marine", "estuarine", "terrestrial")
+
+
+# Filter the habitat descriptions on the different habitat short names
+for (habitat in habitats) {
+    checklist[habitat] <- stringr::str_extract(checklist$habitat,
+                                                   habitat)
+}
+
+# drop the rows without habitat info (for at least any one there is habitat defined)
+checklist <- checklist[rowSums(is.na(checklist[habitats])) < 4,]
+
+# Create the period sections
+bin_years <- 15
+checklist$period_sections <- cut(checklist$firstObservationYearBE,
+                                 seq(1800, 2030, bin_years),
+                                 include.lowest = TRUE, ordered_result = TRUE)
+
+# years earlier to given
+min_year <- 1800
+checklist[checklist$firstObservationYearBE < min_year,
+          "firstObservationYearBE"] <- min_year
+
+# Combine the habitat columns to a single variable column
+checklist <- checklist %>% gather(habitat_short, habitat_name,
+                                  c(freshwater, marine,
+                                    estuarine, terrestrial)) %>%
+                            select(-habitat_short) %>%
+                            drop_na()
+
+# Sort on the year values
+checklist <- checklist[order(checklist$firstObservationYearBE), ]
+
+# combine the period section for every 15 years HIEERDOOR TEVEEL!!
+checklist$period_count <- ave(rep(1, nrow(checklist)),
+                           checklist$period_sections,
+                           checklist$habitat_name,
+                           FUN = sum)
+
+# count number of species in each habitat-period combination
+checklist_count <- count(checklist, c("habitat_name", "period_sections"))
+
+# create cumulative counts per factor
+checklist_count$cumcount <- ave(checklist_count$freq,
+                                checklist_count$habitat_name,
+                                FUN = cumsum)
+#checklist_count$habitat_name <- as.factor(checklist_count$habitat_name)
+
+
+# Fill in the remaining habitat-period combination
+# strategy: combine with a dataframe with all combinations and fill in
+# empty spots
+levels(checklist_count$period_sections) <- seq(min_year, 2020, bin_years)
+checklist_count$habitat_name <- factor(checklist_count$habitat_name)
+
+all_options <- with(checklist_count,
+                    expand.grid(period_sections = levels(checklist_count$period_sections),
+                                habitat_name = levels(habitat_name)))
+all_options <- all_options[order(all_options$period_sections),]
+
+checklist_count_all <- merge(all_options, checklist_count,
+                             by = c("habitat_name", "period_sections"),
+                             all.x = TRUE)
+
+checklist_count_all <- ddply(checklist_count_all, .(habitat_name), na.locf)
+checklist_count_all$cumcount <- as.numeric(checklist_count_all$cumcount)
+
+
+ggplot(data = checklist_count_all, aes(x = period_sections,
+                                   y = cumcount,
+                                   fill = habitat_name)) +
+                geom_bar(stat = "identity") +
+                theme_inbo2015(base_size = 14) +
+                facet_grid(habitat_name ~ .) +
+                scale_x_discrete(labels = seq(1800, 2030, bin_years))
+
+# -----------------------
+
+
+
+
+
+
+
+
+
+
+
 
 ## Cumulative histogram ->
 ## cfr. ISSUE as count is also in between groups...
 ## http://dantalus.github.io/2015/08/16/step-plots/
-# ggplot(data = checklist, aes(firstObservationYearBE)) +
-#     geom_histogram(aes(y = cumsum(..count..),
-#                        fill = habitat_short),
-#                     binwidth = 15,
-#                     position = "stack") +
-#     xlab("") +
-#     ylab("Aantal uitheemse soorten") +
-#     theme_inbo2015(base_size = 14) +
-#     theme(legend.position = "top",
-#           legend.title = element_blank())
+ggplot(data = checklist, aes(firstObservationYearBE,
+                             colour = habitat_short)) +
+    geom_histogram(aes(y = cumsum(..count..),
+                       fill = habitat_short),
+                    binwidth = 15,
+                    position = "stack") +
+    xlab("") +
+    ylab("Aantal uitheemse soorten") +
+    theme_inbo2015(base_size = 14) +
+    theme(legend.position = "top",
+          legend.title = element_blank())  +
+    facet_grid(.~ habitat_short)
 
 # Possible SOLUTION 1: plot them each independent
 
-bin_years = 15
+
 ggplot(data = checklist, aes(firstObservationYearBE)) +
     stat_bin(data = subset(checklist, habitat_short == "terrestrial"),
              aes(y = cumsum(..count..)),
@@ -150,7 +239,7 @@ ggplot(data = checklist, aes(firstObservationYearBE)) +
              binwidth = bin_years,
              geom = "step", size = 3, color = INBOblue) +
     annotate("text",
-             x = max(checklist$firstObservationYearBE)+ 1.,
+             x = max(checklist$firstObservationYearBE) + 1.,
              y = sum(checklist$habitat_short == "freshwater"),
              label = as.character(sum(checklist$habitat_short == "freshwater")),
              size = 8, color = INBOblue) +
@@ -160,7 +249,7 @@ ggplot(data = checklist, aes(firstObservationYearBE)) +
                    geom = "step", size = 3, color = INBOreddishbrown) +
     annotate("text",
              x = max(checklist$firstObservationYearBE) + 1.,
-             y = sum(checklist$habitat_short == "marine"),
+             y = sum(checklist$habitat_short == "marine") - 3,
              label = as.character(sum(checklist$habitat_short == "marine")),
              size = 8, color = INBOreddishbrown) +
     stat_bin(data = subset(checklist, habitat_short == "estuarine"),
@@ -172,6 +261,11 @@ ggplot(data = checklist, aes(firstObservationYearBE)) +
              y = sum(checklist$habitat_short == "estuarine"),
              label = as.character(sum(checklist$habitat_short == "estuarine")),
              size = 8, color = INBOdarkgreen) +
+    annotate("text",
+             x = max(checklist$firstObservationYearBE) - 3,
+             y = max(table(checklist$habitat_short)) + 4,
+             label = paste("Totaal: ", as.character(nrow(checklist))),
+             size = 8, color = INBOred) +
     xlab("") +
     ylab("Aantal uitheemse soorten") +
     theme_inbo2015(base_size = 14) +
@@ -179,25 +273,27 @@ ggplot(data = checklist, aes(firstObservationYearBE)) +
           legend.title = element_blank(),
           legend.text = element_text((size = 14)))
 
-
-
-
-# Consider to calculate it manually and plot the 'bars'...
-
-
-
-# other try-out...
-library(plyr)
-checklist <- ddply(checklist,.(habitat_short),transform,
-                   len = length(firstObservationYearBE))
-ggplot(data = checklist, aes(firstObservationYearBE,
-                             fill = habitat_short,
-                             color = habitat_short)) +
-    geom_step(aes(len = len, y = ..y.. * len),
-              stat="ecdf", bin = 15)
-
-ggsave("cumul_number.png", width = 30, height = 33,
+ggsave("cumulatief_aantal_invasieve.png", width = 33, height = 25,
        units = "cm", dpi = 150)
+
+
+# SOLUTION 2: MANUEEL CUMULATIEIF EN PLOTTEN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
